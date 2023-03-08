c910bc890e0eb7e21883d2a24a785f39646b40b1
John Vickery <jnvicker@ncsu.edu>
2023-03-08 16:02:15 -0500

    corrected for no data error and clarified df cols

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def load_data():
             "online_duke": int,
             "online_nccu": int,
             "online_unc": int,
+            "TRLN_dup_pct": float,
         },
     )
     df["upload_year"] = pd.to_datetime(df["upload_date"]).dt.year
@@ -38,7 +39,7 @@ def load_data():
         "link",
     ]
     df.drop(columns=drop_cols, inplace=True)
-    df.filter(like="print_", axis=1)
+    # df.filter(like="print_", axis=1)
     return df
 
 
@@ -76,162 +77,172 @@ with row0_1:
         format_func=upper_school,
     )
 
-# first row of barcharts
-row1_1, row1_2 = st.columns(2)
+try:
+    # first row of barcharts
+    row1_1, row1_2 = st.columns(2)
 
-# barchart of upload year by dup flag for school
-with row1_1:
-    year_by_dup = pd.crosstab(df["upload_year"], df[f"{school}_dup_flag"])
-    year_by_dup.rename(columns={0: "No", 1: "Yes"}, inplace=True)
-    year_by_dup.reset_index(inplace=True)
-    fig = px.bar(
-        year_by_dup,
-        x="upload_year",
-        y=["No", "Yes"],
-        title=f"Duplicates by year for {school.upper()}",
-        barmode="group",
-    )
-    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-
-# barchart of upload year by TRLN dup pct
-with row1_2:
-    year_by_dup_pct = pd.DataFrame(
-        df[["upload_year", "TRLN_dup_pct"]].value_counts().reset_index()
-    )
-    year_by_dup_pct.rename(columns={0: "Count"}, inplace=True)
-    year_by_dup_pct.sort_values(by=["upload_year", "TRLN_dup_pct"])
-    year_by_dup_pct["TRLN_dup_pct"] = year_by_dup_pct["TRLN_dup_pct"].astype(str)
-    fig1 = px.bar(
-        year_by_dup_pct,
-        x="upload_year",
-        y="Count",
-        title="TRLN duplicate percentage by year",
-        color="TRLN_dup_pct",
-    )
-    st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
-
-# second row of charts
-row2_1, row2_2 = st.columns(2)
-
-with row2_1:
-    # Top 10 modules with most dups
-    top_dup_modules = (
-        df.groupby("module")[f"{school}_dup_flag"]
-        .sum()
-        .to_frame("Number of duplicates")
-        .sort_values(by="Number of duplicates", ascending=False)
-        .reset_index()
-    )
-    fig3 = px.bar(
-        top_dup_modules.head(10),
-        x="Number of duplicates",
-        y="module",
-        orientation="h",
-        title=f"Top 10 modules with the most duplication for {school.upper()}",
-    )
-    fig3.update_layout(yaxis=dict(autorange="reversed"))
-    st.plotly_chart(fig3, theme="streamlit", use_container_width=True)
-
-with row2_2:
-    # Top 10 press with most dups
-    top_dup_press = (
-        df.groupby("press")[f"{school}_dup_flag"]
-        .sum()
-        .to_frame("Number of duplicates")
-        .sort_values(by="Number of duplicates", ascending=False)
-        .reset_index()
-    )
-    fig4 = px.bar(
-        top_dup_press.head(10),
-        x="Number of duplicates",
-        y="press",
-        orientation="h",
-        title=f"Top 10 presses with the most duplication for {school.upper()}",
-    )
-    fig4.update_layout(yaxis=dict(autorange="reversed"))
-    st.plotly_chart(fig4, theme="streamlit", use_container_width=True)
-
-# third row of barcharts
-row3_1, row3_2 = st.columns(2)
-with row3_1:
-    if school == "TRLN":
-        dups = df.loc[df["TRLN_dup_flag"] == 1].copy()
-        cols2keep = [
-            "upload_year",
-            "print_duke",
-            "print_nccu",
-            "print_ncsu",
-            "print_unc",
-            "online_duke",
-            "online_nccu",
-            "online_ncsu",
-            "online_unc",
-        ]
-        dups = dups[cols2keep]
-        dups["print_total"] = dups.filter(regex="print_").sum(axis=1)
-        dups["ebook_total"] = dups.filter(regex="online_").sum(axis=1)
-        dups.loc[
-            (dups["print_total"] > 0) & (dups["ebook_total"] > 1), "dup_format"
-        ] = "Print + Ebook"
-        dups.loc[
-            (dups["print_total"] >= 1) & (dups["ebook_total"] <= 4), "dup_format"
-        ] = "Print"
-        dups.loc[
-            (dups["print_total"] == 0) & (dups["ebook_total"] >= 4), "dup_format"
-        ] = "Ebook"
-    else:
-        dups = (
-            df[["upload_year", f"print_{school}", f"online_{school}"]]
-            .loc[df[f"{school}_dup_flag"] == 1]
-            .copy()
+    # barchart of upload year by dup flag for school
+    with row1_1:
+        year_by_dup = pd.crosstab(df["upload_year"], df[f"{school}_dup_flag"])
+        year_by_dup.rename(columns={0: "No", 1: "Yes"}, inplace=True)
+        year_by_dup.reset_index(inplace=True)
+        fig = px.bar(
+            year_by_dup,
+            x="upload_year",
+            y=["No", "Yes"],
+            title=f"Duplicates by year for {school.upper()}",
+            barmode="group",
         )
-        dups.loc[
-            (dups[f"online_{school}"] > 1) & (dups[f"print_{school}"] > 0), "dup_format"
-        ] = "Print + Ebook"
-        dups.loc[
-            (dups[f"online_{school}"] == 1) & (dups[f"print_{school}"] > 0),
-            "dup_format",
-        ] = "Print"
-        dups.loc[
-            (dups[f"online_{school}"] > 1) & (dups[f"print_{school}"] == 0),
-            "dup_format",
-        ] = "Ebook"
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-    dups_bar = (
-        dups.groupby(["upload_year", "dup_format"]).size().to_frame().reset_index()
-    )
-    dups_bar.rename(columns={0: "Count"}, inplace=True)
-    fig5 = px.bar(
-        dups_bar,
-        x="upload_year",
-        y="Count",
-        color="dup_format",
-        title=f"Duplicates by format and year for {school.upper()}",
-        barmode="group",
-    )
-    st.plotly_chart(fig5, theme="streamlit", use_container_width=True)
+    # barchart of upload year by TRLN dup pct
+    with row1_2:
+        year_by_dup_pct = pd.DataFrame(
+            df[["upload_year", "TRLN_dup_pct"]].value_counts().reset_index()
+        )
+        year_by_dup_pct.rename(columns={0: "Count"}, inplace=True)
+        year_by_dup_pct.sort_values(by=["upload_year", "TRLN_dup_pct"])
+        year_by_dup_pct["TRLN_dup_pct"] = year_by_dup_pct["TRLN_dup_pct"].astype(str)
+        fig1 = px.bar(
+            year_by_dup_pct,
+            x="upload_year",
+            y="Count",
+            title="TRLN duplicate percentage by year",
+            color="TRLN_dup_pct",
+        )
+        st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
 
-with row3_2:
-    if school == "TRLN":
-        no_copies = df[cols2keep]
-        no_copies["print_total"] = no_copies.filter(regex="print_").sum(axis=1)
-        no_copies["ebook_total"] = no_copies.filter(regex="online_").sum(axis=1)
-        no_copies = no_copies.loc[
-            (no_copies["print_total"] == 0) & (no_copies["ebook_total"] == 0)
-        ]
-    else:
-        no_copies = df.loc[
-            (df[f"print_{school}"] == 0) & (df[f"online_{school}"] == 0)
-        ].copy()
-    no_copies = no_copies["upload_year"].value_counts().to_frame().reset_index()
-    no_copies.columns = ["upload_year", "num_missing"]
-    fig6 = px.bar(
-        no_copies,
-        x="upload_year",
-        y="num_missing",
-        title=f"Number of titles missing per upload year for {school.upper()}",
+    # second row of charts
+    row2_1, row2_2 = st.columns(2)
+
+    with row2_1:
+        # Top 10 modules with most dups
+        top_dup_modules = (
+            df.groupby("module")[f"{school}_dup_flag"]
+            .sum()
+            .to_frame("Number of duplicates")
+            .sort_values(by="Number of duplicates", ascending=False)
+            .reset_index()
+        )
+        fig3 = px.bar(
+            top_dup_modules.head(10),
+            x="Number of duplicates",
+            y="module",
+            orientation="h",
+            title=f"Top 10 modules with the most duplication for {school.upper()}",
+        )
+        fig3.update_layout(yaxis=dict(autorange="reversed"))
+        st.plotly_chart(fig3, theme="streamlit", use_container_width=True)
+
+    with row2_2:
+        # Top 10 press with most dups
+        top_dup_press = (
+            df.groupby("press")[f"{school}_dup_flag"]
+            .sum()
+            .to_frame("Number of duplicates")
+            .sort_values(by="Number of duplicates", ascending=False)
+            .reset_index()
+        )
+        fig4 = px.bar(
+            top_dup_press.head(10),
+            x="Number of duplicates",
+            y="press",
+            orientation="h",
+            title=f"Top 10 presses with the most duplication for {school.upper()}",
+        )
+        fig4.update_layout(yaxis=dict(autorange="reversed"))
+        st.plotly_chart(fig4, theme="streamlit", use_container_width=True)
+
+    # third row of barcharts
+    row3_1, row3_2 = st.columns(2)
+    with row3_1:
+        if school == "TRLN":
+            dups = df.loc[df["TRLN_dup_flag"] == 1].copy()
+            cols2keep = [
+                "upload_year",
+                "print_duke",
+                "print_nccu",
+                "print_ncsu",
+                "print_unc",
+                "online_duke",
+                "online_nccu",
+                "online_ncsu",
+                "online_unc",
+            ]
+            dups = dups[cols2keep]
+            dups["print_total"] = dups.filter(regex="print_").sum(axis=1)
+            dups["ebook_total"] = dups.filter(regex="online_").sum(axis=1)
+            dups.loc[
+                (dups["print_total"] > 0) & (dups["ebook_total"] > 1), "dup_format"
+            ] = "Print + Ebook"
+            dups.loc[
+                (dups["print_total"] >= 1) & (dups["ebook_total"] <= 4), "dup_format"
+            ] = "Print"
+            dups.loc[
+                (dups["print_total"] == 0) & (dups["ebook_total"] >= 4), "dup_format"
+            ] = "Ebook"
+        else:
+            dups = (
+                df[["upload_year", f"print_{school}", f"online_{school}"]]
+                .loc[df[f"{school}_dup_flag"] == 1]
+                .copy()
+            )
+            dups.loc[
+                (dups[f"online_{school}"] > 1) & (dups[f"print_{school}"] > 0),
+                "dup_format",
+            ] = "Print + Ebook"
+            dups.loc[
+                (dups[f"online_{school}"] == 1) & (dups[f"print_{school}"] > 0),
+                "dup_format",
+            ] = "Print"
+            dups.loc[
+                (dups[f"online_{school}"] > 1) & (dups[f"print_{school}"] == 0),
+                "dup_format",
+            ] = "Ebook"
+
+        dups_bar = (
+            dups.groupby(["upload_year", "dup_format"]).size().to_frame().reset_index()
+        )
+        dups_bar.rename(columns={0: "Count"}, inplace=True)
+        fig5 = px.bar(
+            dups_bar,
+            x="upload_year",
+            y="Count",
+            color="dup_format",
+            title=f"Duplicates by format and year for {school.upper()}",
+            barmode="group",
+        )
+        st.plotly_chart(fig5, theme="streamlit", use_container_width=True)
+
+    with row3_2:
+        if school == "TRLN":
+            no_copies = df[cols2keep]
+            no_copies["print_total"] = no_copies.filter(regex="print_").sum(axis=1)
+            no_copies["ebook_total"] = no_copies.filter(regex="online_").sum(axis=1)
+            no_copies = no_copies.loc[
+                (no_copies["print_total"] == 0) & (no_copies["ebook_total"] == 0)
+            ]
+        else:
+            no_copies = df.loc[
+                (df[f"print_{school}"] == 0) & (df[f"online_{school}"] == 0)
+            ].copy()
+        no_copies = no_copies["upload_year"].value_counts().to_frame().reset_index()
+        no_copies.columns = ["upload_year", "num_missing"]
+        fig6 = px.bar(
+            no_copies,
+            x="upload_year",
+            y="num_missing",
+            title=f"Number of titles missing per upload year for {school.upper()}",
+        )
+        st.plotly_chart(fig6, theme="streamlit", use_container_width=True)
+except ValueError:
+    st.subheader(
+        (
+            f"{school.upper()} does not have enough "
+            f"data for {year[0]} to {year[1]} to show graphs. "
+            "Select a wider year range."
+        )
     )
-    st.plotly_chart(fig6, theme="streamlit", use_container_width=True)
 
 # Data table and download option
 st.subheader(
@@ -252,4 +263,41 @@ st.download_button(
     "text/csv",
     key="download-csv",
 )
-st.dataframe(df)
+
+# format dataframe for display
+# map "Yes" to 1 and "No" to 0 for dup flag columns
+dup_flag_cols = df.filter(regex="_dup_flag")
+dup_flag_cols = dup_flag_cols.applymap(lambda x: "Yes" if x == 1 else "No")
+df[dup_flag_cols.columns] = dup_flag_cols
+
+# add percent sign to TRLN_dup_pct
+df["TRLN_dup_pct"] = (df["TRLN_dup_pct"] * 100).map("{:,.0f}%".format)
+
+# rename columns for clarity
+display_df = df.rename(
+    columns={
+        "title": "Title",
+        "module": "OUP Module",
+        "print_pubdate": "Print pub. year",
+        "isbn": "ISBN",
+        "eisbn": "EISBN",
+        "press": "Press",
+        "online_ncsu": "Online records NCSU",
+        "print_ncsu": "Print records NCSU",
+        "online_duke": "Online records Duke",
+        "print_duke": "Print records Duke",
+        "online_unc": "Online records UNC",
+        "print_unc": "Print records UNC",
+        "online_nccu": "Online records NCCU",
+        "print_nccu": "Print records NCCU",
+        "duke_dup_flag": "Dups at Duke",
+        "ncsu_dup_flag": "Dups at NCSU",
+        "unc_dup_flag": "Dups at UNC",
+        "nccu_dup_flag": "Dups at NCCU",
+        "TRLN_dup_pct": "Pct of TRLN with dups",
+        "TRLN_dup_flag": "Dups in TRLN",
+        "upload_year": "OUP upload year",
+    }
+)
+
+st.dataframe(display_df)
